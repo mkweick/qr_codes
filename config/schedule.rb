d9204dbec2627d5aec8c19dce7e576cff7d6130d
Matt Weick <mkweick@gmail.com>
2016-08-31 06:55:17 -0700

    comment out production job until rollout

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,10 @@
 set :output, { :error => "log/cron_error_log.log",
                :standard => "log/cron_log.log" }
 
-# every '* * * * *' do
-#   rake "events:clear_deleted_dev", environment: "development"
-# end
-
-every '0 4 * * *' do
-  rake "events:clear_deleted_prod", environment: "production"
+every '* * * * *' do
+  rake "events:clear_deleted_dev", environment: "development"
 end
+
+# every '0 4 * * *' do
+#   rake "events:clear_deleted_prod", environment: "production"
+# end

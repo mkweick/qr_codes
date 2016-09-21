ceaea2ac000de51f04cbe53f2380b4a7b88417f1
Matt Weick <mkweick@gmail.com>
2016-09-20 21:07:46 -0400

    add batch location to notification email if present

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,6 +3,17 @@ class NotificationMailer < ApplicationMailer
 	def batch_generation_complete_email(event, batch, email)
 		@event = event
 		@batch = batch
-		mail(to: email, subject: "#{@event.name} - Batch #{@batch.number} is Ready!")
+
+    if @batch.location
+      mail(
+        to: email,
+        subject: "#{@event.name} - #{@batch.location} - Batch #{@batch.number} is Ready!"
+      )
+    else
+		  mail(
+        to: email,
+        subject: "#{@event.name} - Batch #{@batch.number} is Ready!"
+      )
+    end
 	end
 end

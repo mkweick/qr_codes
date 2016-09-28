d7a88df0a46d7ffd4558a40d94cf6cb3b5278b1a
Matt Weick <mkweick@gmail.com>
2016-09-28 12:58:51 +0000

    change QR code size to 1.25 inches

--- app/controllers/on_site_attendees_controller.rb
+++ app/controllers/on_site_attendees_controller.rb
@@ -41,7 +41,7 @@ class OnSiteAttendeesController < ApplicationController
       "#{"\n" + @attendee.email if @attendee.email}" +
       "#{"\n" + @attendee.phone if @attendee.phone}" +
       "#{"\n" + @attendee.salesrep if @attendee.salesrep};;", level: :q
-    ).to_img.resize(200, 200)
+    ).to_img.resize(180, 180)
 
     render layout: false
   end

73dd4641768b912f50780e7ab707e9b507324471
Matt Weick <mkweick@gmail.com>
2016-11-22 08:59:21 -0500

    refactor subject line

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,31 +4,16 @@ class NotificationMailer < ApplicationMailer
 		@event = event
 		@batch = batch
 
-    if @batch.location
-      if @batch.batch_type == '1'
-        mail(
-          to: email,
-          subject: "#{@event.name} - #{@batch.location} - Batch " +
-            "#{@batch.number} - Attendees is Ready!"
-        )
-      elsif @batch.batch_type == '2'
-        mail(
-          to: email,
-          subject: "#{@event.name} - Batch #{@batch.number} - Employees is Ready!"
-        )
-      end
-    else
-		  if @batch.batch_type == '1'
-        mail(
-          to: email,
-          subject: "#{@event.name} - Batch #{@batch.number} - Attendees is Ready!"
-        )
-      elsif @batch.batch_type == '2'
-        mail(
-          to: email,
-          subject: "#{@event.name} - Batch #{@batch.number} - Employees is Ready!"
-        )
-      end
+    if @batch.batch_type == '1'
+      mail(to: email,
+        subject: "#{@event.name}#{' - ' + @batch.location if @batch.location}" +
+          " - Batch #{@batch.number} - Attendees is Ready!"
+      )
+    elsif @batch.batch_type == '2'
+      mail(
+        to: email,
+        subject: "#{@event.name} - Batch #{@batch.number} - Employees is Ready!"
+      )
     end
 	end
 
@@ -38,8 +23,7 @@ class NotificationMailer < ApplicationMailer
     @last_name = ln
     @account_name = an
 
-    mail(
-      from: 'Customer Arrived <noreply@divalsafety.com>', to: email,
+    mail(from: 'Customer Arrived <noreply@divalsafety.com>', to: email,
       subject: "#{@first_name if @first_name}" +
         "#{' ' + @last_name if @last_name}" +
         "#{' - ' + @account_name if @account_name}"

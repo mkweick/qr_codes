d47b715fac8a1a88aa0b8b51b59c84c516305d40
Matt Weick <mkweick@gmail.com>
2016-11-21 22:46:07 -0500

    refactor redundant sql scripts

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -75,104 +75,32 @@ class CheckInsController < ApplicationController
         host: ENV["CRM_DB_HOST"], database: ENV["CRM_DB_NAME"],
         username: ENV["CRM_DB_UN"], password: ENV["CRM_DB_PW"]
       )
+
+      sql = registered_attendees_base_sql_script
     end
 
     if first_name
-      params.delete(:last_name)
-      params.delete(:account_name)
       first_name = db.escape(first_name)
-
-      sql = "SELECT a.ActivityId AS \"activity_id\", " +
-        "e.InternalEMailAddress AS \"salesrep_email\", " +
-        "a.ResponseCode AS \"response_code\", " + 
-        "d.FirstName AS \"contact_first_name\", " +
-        "d.LastName AS \"contact_last_name\", " +
-        "d.ParentCustomerIdName AS \"contact_account_name\", " +
-        "a.FirstName AS \"first_name\", a.LastName AS \"last_name\", " +
-        "a.CompanyName AS \"account_name\" " +
-        "FROM ActivityPointerBase AS a " +
-        "JOIN CampaignBase AS b ON b.CampaignId = a.RegardingObjectId " +
-        "JOIN CampaignResponseBase AS c ON c.ActivityId = a.ActivityId " +
-        "LEFT JOIN ContactBase AS d ON d.ContactId = c.new_Contact " +
-        "LEFT JOIN SystemUserBase AS e ON e.SystemUserId = a.OwnerId " +
-        "WHERE a.ActivityTypeCode = '4401' " +
-        "AND b.CodeName IN ("
-
-      @event.crm_campaigns.pluck(:code).each_with_index do |code, idx|
-        idx == 0 ? (sql += "\'#{code}\'") : (sql += ", \'#{code}\'")
-      end
-      sql += ") AND (d.FirstName LIKE '#{first_name}%' OR a.FirstName LIKE '#{first_name}%')"
+      sql += "AND (d.FirstName LIKE '#{first_name}%' OR a.FirstName LIKE '#{first_name}%')"
       sql += "ORDER BY d.FirstName, d.LastName;"
-
-      query = db.execute(sql)
-      query.each(as: :array) { |row| @results << row }
-      db.close unless db.closed?
-
     elsif last_name
-      params.delete(:first_name)
-      params.delete(:account_name)
       last_name = db.escape(last_name)
-
-      sql = "SELECT a.ActivityId AS \"activity_id\", " +
-        "e.InternalEMailAddress AS \"salesrep_email\", " +
-        "a.ResponseCode AS \"response_code\", " + 
-        "d.FirstName AS \"contact_first_name\", " +
-        "d.LastName AS \"contact_last_name\", " +
-        "d.ParentCustomerIdName AS \"contact_account_name\", " +
-        "a.FirstName AS \"first_name\", a.LastName AS \"last_name\", " +
-        "a.CompanyName AS \"account_name\" " +
-        "FROM ActivityPointerBase AS a " +
-        "JOIN CampaignBase AS b ON b.CampaignId = a.RegardingObjectId " +
-        "JOIN CampaignResponseBase AS c ON c.ActivityId = a.ActivityId " +
-        "LEFT JOIN ContactBase AS d ON d.ContactId = c.new_Contact " +
-        "LEFT JOIN SystemUserBase AS e ON e.SystemUserId = a.OwnerId " +
-        "WHERE a.ActivityTypeCode = '4401' " +
-        "AND b.CodeName IN ("
-          
-      @event.crm_campaigns.pluck(:code).each_with_index do |code, idx|
-        idx == 0 ? (sql += "\'#{code}\'") : (sql += ", \'#{code}\'")
-      end
-      sql += ") AND (d.LastName LIKE '#{last_name}%' OR a.LastName LIKE '#{last_name}%')"
+      sql += "AND (d.LastName LIKE '#{last_name}%' OR a.LastName LIKE '#{last_name}%')"
       sql += "ORDER BY d.LastName, d.FirstName;"
-
-      query = db.execute(sql)
-      query.each(as: :array) { |row| @results << row }
-      db.close unless db.closed?
-
     elsif account_name
       if account_name.size > 2
-        params.delete(:first_name)
-        params.delete(:last_name)
         account_name = db.escape(account_name)
-
-        sql = "SELECT a.ActivityId AS \"activity_id\", " +
-          "e.InternalEMailAddress AS \"salesrep_email\", " +
-          "a.ResponseCode AS \"response_code\", " + 
-          "d.FirstName AS \"contact_first_name\", " +
-          "d.LastName AS \"contact_last_name\", " +
-          "d.ParentCustomerIdName AS \"contact_account_name\", " +
-          "a.FirstName AS \"first_name\", a.LastName AS \"last_name\", " +
-          "a.CompanyName AS \"account_name\" " +
-          "FROM ActivityPointerBase AS a " +
-          "JOIN CampaignBase AS b ON b.CampaignId = a.RegardingObjectId " +
-          "JOIN CampaignResponseBase AS c ON c.ActivityId = a.ActivityId " +
-          "LEFT JOIN ContactBase AS d ON d.ContactId = c.new_Contact " +
-          "LEFT JOIN SystemUserBase AS e ON e.SystemUserId = a.OwnerId " +
-          "WHERE a.ActivityTypeCode = '4401' " +
-          "AND b.CodeName IN ("
-          
-        @event.crm_campaigns.pluck(:code).each_with_index do |code, idx|
-          idx == 0 ? (sql += "\'#{code}\'") : (sql += ", \'#{code}\'")
-        end
-        sql += ") AND (d.ParentCustomerIdName LIKE '%#{account_name}%' OR a.CompanyName LIKE '%#{account_name}%')"
+        sql += "AND (d.ParentCustomerIdName LIKE '%#{account_name}%' OR a.CompanyName LIKE '%#{account_name}%')"
         sql += "ORDER BY d.ParentCustomerIdName, d.FirstName;"
-
-        query = db.execute(sql)
-        query.each(as: :array) { |row| @results << row }
-        db.close unless db.closed?
       else
         @account_length_error = true
       end
+    end
+
+    if db
+      query = db.execute(sql)
+      query.each(as: :array) { |row| @results << row }
+      db.close unless db.closed?
     end
   end
 
@@ -187,5 +115,30 @@ class CheckInsController < ApplicationController
       flash.alert = "Check-In not available. Event has no assigned CRM Campaigns."
       redirect_to root_path
     end
+  end
+
+  def registered_attendees_base_sql_script
+    base_sql_statement = "SELECT a.ActivityId AS \"activity_id\", " +
+      "e.InternalEMailAddress AS \"salesrep_email\", " +
+      "a.ResponseCode AS \"response_code\", " + 
+      "d.FirstName AS \"contact_first_name\", " +
+      "d.LastName AS \"contact_last_name\", " +
+      "d.ParentCustomerIdName AS \"contact_account_name\", " +
+      "a.FirstName AS \"first_name\", a.LastName AS \"last_name\", " +
+      "a.CompanyName AS \"account_name\" " +
+      "FROM ActivityPointerBase AS a " +
+      "JOIN CampaignBase AS b ON b.CampaignId = a.RegardingObjectId " +
+      "JOIN CampaignResponseBase AS c ON c.ActivityId = a.ActivityId " +
+      "LEFT JOIN ContactBase AS d ON d.ContactId = c.new_Contact " +
+      "LEFT JOIN SystemUserBase AS e ON e.SystemUserId = a.OwnerId " +
+      "WHERE a.ActivityTypeCode = '4401' " +
+      "AND b.CodeName IN ("
+
+    @event.crm_campaigns.pluck(:code).each_with_index do |code, idx|
+      base_sql_statement += idx == 0 ? "\'#{code}\'" : ", \'#{code}\'"
+    end
+    
+    base_sql_statement += ') '
+    base_sql_statement
   end
 end

beea9ded9d03f61fcdd6160ea2e65dccbe283d48
Matt Weick <mkweick@gmail.com>
2017-01-03 23:25:43 -0500

    allow all staff to login and check for admin access

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,10 @@ class SessionsController < ApplicationController
     redirect_to login_path unless params[:username] && params[:password]
 
     username, password = params[:username], params[:password]
-    ldap_user = Adauth.authenticate(username, password)
+    user = Adauth.authenticate(username, password)
 
-    if ldap_user
-      session[:email] = ldap_user.ldap_object[:mail].first.strip
-      session[:first_name] = ldap_user.ldap_object[:givenname].first.strip
+    if user
+      set_session_params(user)
       redirect_to(session[:return_to] || root_path)
       session.delete(:return_to)
     else 
@@ -23,10 +22,44 @@ class SessionsController < ApplicationController
 
   def destroy
     if logged_in?
+      session.delete(:name)
       session.delete(:email)
-      session.delete(:first_name)
+      session.delete(:guid)
+      session.delete(:access)
     end
+
     flash.notice = 'Successfully logged out'
     redirect_to login_path
+  end
+
+  private
+
+  def set_session_params(user)
+    user_params = user.ldap_object
+    email = user_params[:mail].first
+    member_of = user.cn_groups_nested
+    access_level = member_of.include?('Events QR Admin Access') ? '1' : '0'
+
+    session[:name] = user_params[:givenname].first || 'User'
+    session[:email] = email
+    session[:guid] = crm_user_id(email)
+    session[:access] = access_level
+  end
+
+  def crm_user_id(email)
+    if email
+      db = crm_connection_sql
+      email = db.escape(email)
+      sql = "SELECT SystemUserId FROM SystemUserBase " +
+        "WHERE InternalEmailAddress = '#{email}'"
+      
+      query = db.execute(sql)
+      user = query.each(:symbolize_keys => true)
+      db.close unless db.closed?
+
+      user.any? ? user.first[:SystemUserId] : nil
+    else
+      nil
+    end
   end
 end
